4cd70b85a4cf4fe8a44701b8cf8ed766f022f7ba
koshisaki <saki.lalasong@gmail.com>
2021-09-19 20:57:54 +0900

    add config

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,9 @@ module Houseworkapp
 
     # 日本語の言語設定
     config.i18n.default_locale = :ja
+
     config.assets.initialize_on_precompile = false
+    config.assets.compile = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
