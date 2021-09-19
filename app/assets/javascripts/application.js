49279547b46df6c47e14f85d82a233e80eeff29f
koshisaki <saki.lalasong@gmail.com>
2021-09-19 21:15:11 +0900

    bugfix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
-// = require data-confirm-modal
+//= require data-confirm-modal
 //= require bootstrap-sprockets
 //= require_tree .
